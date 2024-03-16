193f767db6318c428eb7e2d656750f6032e83d3e
Danilo Benedetti Ribeiro <danilobenedetti1@gmail.com>
2024-03-16 11:06:04 -0300

    (16/03) primeiro teste conexao MongoDB

--- back-end/src/controllers/cliente.js
+++ back-end/src/controllers/cliente.js
@@ -1,5 +1,19 @@
 import prisma from '../database/client.js'
 
-const controller = {}
+const controller = {} //objeto vazio
+
+controller.create = async function(req,res) {
+    try{
+        await prisma.cliente.create({data: req.body})
+        //Envia uma resposta de sucessoao front-end
+        //HTTP 201: Created
+        res.status(201).end()
+    }
+    catch(error){
+        console.error(error)
+        //HTTP 500: Internal Server Error
+        res.status(500).end()
+    }
+}
 
 export default controller
